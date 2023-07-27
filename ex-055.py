08c66cf1a3a51552b2ac5153d880eb012a999c3b
Pytrick77 <patrickoscar11@gmail.com>
2023-07-27 18:30:53 -0300

    Adicionando outra resolução do exercício 55.

--- ex-055.py
+++ ex-055.py
@@ -1,6 +1,18 @@
 # Exercício de maior e menor pesos de uma sequencia de 6 entradas
 
+maior = 0
+menor = 0
 
+for i in range(1,7):
+    peso = float(input(f'Informe o peso da {i}° Pessoa: '))
+    
+    if peso > maior:
+        maior = peso
+
+    elif menor == 0 or menor > peso:
+        menor = peso
+
+print(f'O maior peso é de {maior:.1f}kg e o menor é de {menor:.1f}kg!')
 
 
 # Outra forma de resolver o exercício com listas.
