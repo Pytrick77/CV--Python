772f7ede12b3f749ba81de43571d00374bceb264
Pytrick77 <patrickoscar11@gmail.com>
2023-07-27 18:22:18 -0300

    Resolução do Exercício 53 adicionada

--- ex-053.py
+++ ex-053.py
@@ -1,2 +1,17 @@
 # Exercício para descobrir se uma frase é ou não um palíndromo
 
+frase = str(input('Digite uma frase: ')).strip().upper()
+frase_separada = frase.replace(' ', '')
+frase_junta = ''
+
+for i in range (len(frase_separada) - 1, -1, -1):
+    frase_junta += frase_separada[i]
+
+
+print(f'A frase {frase_separada} invertida fica {frase_junta}!')
+
+if frase_separada == frase_junta:
+    print('Temos um palíndromo.')
+
+else:
+    print('A frase não é um palíndromo.')
