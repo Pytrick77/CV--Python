e34f047d39e4ad76f8d726eb4a6c1af5983d8bc7
Pytrick77 <patrickoscar11@gmail.com>
2023-08-17 18:38:42 -0300

    Refazendo o exercício 76.

--- ex-076.py
+++ ex-076.py
@@ -2,10 +2,10 @@ print('-'*30)
 print(f'{"Listagem de Preços":^30}')
 print('-'*30)
 
-listagem = 'Arroz', 10.50, 'Macarrão', 3.50, 'Feijão', 8.30, 'Camiseta', 50.00, 'Shorts', 35.70
+listagem = 'Arroz', 10.50, 'Feijão', 8.00, 'Óleo', 7.98, 'Sal', 4.85, 'Açúcar', 7.49
 
-for i in range(0, len(listagem)):
-    if i % 2 == 0:
-        print(f'{listagem[i]:.<15}', end='')
+for itens in range(0, len(listagem)):
+    if itens % 2 == 0:
+        print(f'{listagem[itens]:.<20}', end='')
     else:
-        print(f'{listagem[i]:>10.2f}')
+        print(f'R${listagem[itens]:>7.2f}')
